9317740c72dee418fb0a840b95e6cbe5c390d041
[CLOUD]나용진 <yjnaga@naver.com>
2024-08-19 23:53:42 +0900

    Create anti-classes to resolve errors

--- first_project/src/main/java/com/team5/first_project/comment/entity/Comment.java
+++ first_project/src/main/java/com/team5/first_project/comment/entity/Comment.java
@@ -1,4 +1,33 @@
 package com.team5.first_project.comment.entity;
 
-public class Comment {
+import com.team5.first_project.member.entity.Member;
+import com.team5.first_project.post.entity.Post;
+import com.team5.first_project.timestamp.Timestamp;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Comment extends Timestamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 }
