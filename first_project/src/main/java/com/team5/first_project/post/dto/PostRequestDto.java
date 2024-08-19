fd2864205f50e8ea36cd77536bc17b93fe929bea
[CLOUD]나용진 <yjnaga@naver.com>
2024-08-19 23:22:45 +0900

    post의 dto 파일들 생성

---
+++ first_project/src/main/java/com/team5/first_project/post/dto/PostRequestDto.java
@@ -0,0 +1,18 @@
+package com.team5.first_project.post.dto;
+
+import lombok.Getter;
+
+/*
+    RequestDto
+    클라이언트가 서버로 데이터를 보낼 때, 특히 POST 또는 PUT 요청 시 필요한 데이터를 담음
+    게시글을 생성하거나 수정할 때 사용
+    게시글의 제목, 내용 등 클라이언트에서 서버로 전달해야 할 데이터만 포함
+*/
+
+@Getter
+public class PostRequestDto {
+
+    private String title;
+
+    private String content;
+}
--- first_project/src/main/java/com/team5/first_project/post/repository/PostRepository.java
+++ first_project/src/main/java/com/team5/first_project/post/repository/PostRepository.java
@@ -1,4 +1,9 @@
 package com.team5.first_project.post.repository;
 
-public interface PostRepository {
+import com.team5.first_project.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
