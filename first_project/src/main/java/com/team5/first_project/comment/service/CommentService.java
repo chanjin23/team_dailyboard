af939eea090cae3335f7f5b8e487a94614d883d8
[CLOUD4] 이찬진 <chanjin23@naver.com>
2024-08-20 21:16:04 +0900

    Feat : 댓글 조회 구현

--- first_project/src/main/java/com/team5/first_project/comment/service/CommentService.java
+++ first_project/src/main/java/com/team5/first_project/comment/service/CommentService.java
@@ -1,4 +1,34 @@
 package com.team5.first_project.comment.service;
 
+import com.team5.first_project.comment.entity.Comment;
+import com.team5.first_project.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public Comment createComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public List<Comment> findAllComments() {
+        return commentRepository.findAll();
+    }
+
+    public Comment findComment(long id) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() ->new IllegalArgumentException("해당 댓글이 존재하지 않습니다. id: " + id));
+        return comment;
+    }
+
+    public void deleteComment(long id) {
+        Comment comment = findComment(id);
+        commentRepository.delete(comment);
+    }
 }
