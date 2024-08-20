b6aa3b2f320a949736ca17994e0f42812583c896
[CLOUD]나용진 <yjnaga@naver.com>
2024-08-20 20:49:40 +0900

    create, update(3)

--- first_project/src/main/java/com/team5/first_project/post/controller/PostController.java
+++ first_project/src/main/java/com/team5/first_project/post/controller/PostController.java
@@ -7,23 +7,33 @@ import com.team5.first_project.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
     // 게시글 생성
+    @GetMapping("/posts/create/{id}")
+    public String createPage(@PathVariable("id") long id, Model model){
+        model.addAttribute("boardId", id);
+        return "createPost";
+    }
+
     // @AuthenticationPrincipal
-    @PostMapping("/posts/create/{boardId}")
-    public String createPost(@Valid @RequestBody PostRequestDto postRequestDto){
+    @PostMapping("/posts/create/{id}")
+    public String createPost(@PathVariable("id") long id,
+                             @Valid @ModelAttribute PostRequestDto postRequestDto){
+        // postRequestDto.setBoardId(id);
+        // requestDto에 boardId추가 후 주석 해제
         PostResponseDto postResponseDto = postService.createPost(postRequestDto);
-        return "redirect:/board/" + postResponseDto.getBoardId();
+        return "redirect:/board/";
     }
 
     // 전체 게시글 조회
@@ -53,12 +63,19 @@ public class PostController {
 
 
     // 게시글 수정
+    @GetMapping("/post/{id}/edit")
+    public String editPage(@PathVariable("id") Long id, Model model){
+        Post post = postService.findById(id);
+        model.addAttribute("post", new PostResponseDto[post]);
+        return "editPost";
+    }
+
     // @AuthenticationPrincipal
-    @PatchMapping("/posts/{postId}")
-    public String updatePost(@PathVariable Long postId,
-                                       @Valid @RequestBody PostRequestDto requestDto){
-        PostResponseDto postResponseDto = postService.updatePost(postId, requestDto);
-        return "redirect:/board/" + postResponseDto.getBoardId();
+    @PostMapping("/post/{id}/edit")
+    public String updatePost(@PathVariable("id") Long id,
+                                       @Valid @ModelAttribute PostRequestDto requestDto){
+        postService.updatePost(id, requestDto);
+        return "redirect:/board/";
     }
 
     // 게시글 삭제
