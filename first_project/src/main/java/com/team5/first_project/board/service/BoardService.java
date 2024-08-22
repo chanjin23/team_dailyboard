23ac2753ad461a65ced346a324d50729f9f9bc7c
[CLOUD4] 이찬진 <chanjin23@naver.com>
2024-08-22 14:09:38 +0900

    Refactor : BoardDTO 수정 및 불필요한 코드 제거

--- first_project/src/main/java/com/team5/first_project/board/service/BoardService.java
+++ first_project/src/main/java/com/team5/first_project/board/service/BoardService.java
@@ -1,25 +1,18 @@
 package com.team5.first_project.board.service;
 
-import com.team5.first_project.board.dto.BoardDTO;
 import com.team5.first_project.board.dto.RequestBoardDto;
 import com.team5.first_project.board.dto.ResponseBoardDto;
 import com.team5.first_project.board.entity.Board;
 import com.team5.first_project.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
-
 
     private final BoardRepository boardRepository;
 
@@ -43,7 +36,7 @@ public class BoardService {
     }
 
     /**
-     * 게시판 저장 (Create)
+     * 게시판 생성 (Create)
      */
     @Transactional
     public ResponseBoardDto saveBoard(String name, String description, String type) {
@@ -67,33 +60,8 @@ public class BoardService {
     }
 
     /**
-     * 게시판 타입으로 조회
+     * 게시판 수정
      */
-    @Transactional
-    public List<BoardDTO> getBoardsByType(String type) {
-        return boardRepository.findByType(type).stream()
-                .map(this::convertToDTO)
-                .collect(Collectors.toList());
-    }
-
-    // 엔티티를 DTO로 변환
-    private BoardDTO convertToDTO(Board board) {
-        return new BoardDTO(board.getId(), board.getName(), board.getDescription(), board.getType());
-    }
-
-    // DTO를 엔티티로 변환
-    private Board convertToEntity(BoardDTO boardDTO) {
-        return new Board(boardDTO.getId(), boardDTO.getName(), boardDTO.getDescription(), boardDTO.getType(), null);
-    }
-
-    @Transactional
-    public Board findById(Long id) {
-        return boardRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException(
-                        "해당 게시판은 존재하지 않는 게시판:" + id
-                ));
-    }
-
     @Transactional
     public void updateBoard(Long id, String description, String name, String type) {
 
