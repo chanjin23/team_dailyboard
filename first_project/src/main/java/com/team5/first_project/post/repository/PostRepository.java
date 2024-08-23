4daf1a2397a0fc54af68dd5153afe8dac156e7f3
[CLOUD4] 한현 <gusgus1467@naver.com>
2024-08-23 15:08:21 +0900

    Fix : 각 게시판에서만 게시글이 검색되도록 수정

--- first_project/src/main/java/com/team5/first_project/post/repository/PostRepository.java
+++ first_project/src/main/java/com/team5/first_project/post/repository/PostRepository.java
@@ -13,7 +13,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     //Page<Post> findAll(Long id, Pageable pageable);
     Page<Post> findAllByBoard(Board board, Pageable pageable);
 
-    Page<Post> findByBoardAndTitleContainingOrContentContaining(Board board, String keyword1, String keyword2, Pageable pageable);
+    Page<Post> findByBoardAndTitleContainingOrBoardAndContentContaining(Board board1, String keyword1, Board board2, String keyword2, Pageable pageable);
 
     //@Query("SELECT p FROM Post p WHERE p.board.id = :boardId AND (p.title LIKE %:keyword1% OR p.content LIKE %:keyword2%)")
     //Page<Post> findByBoardIdAndTitleContainingOrContentContaining(@Param("boardId") Long boardId, String keyword1, String keyword2, Pageable pageable);
