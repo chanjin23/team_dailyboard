9eb3394b3d4b60eff3272b347da3fe90568a147a
[CLOUD]나용진 <yjnaga@naver.com>
2024-08-28 11:58:22 +0900

    format LocalDateTime

--- first_project/src/main/java/com/team5/first_project/timestamp/Timestamp.java
+++ first_project/src/main/java/com/team5/first_project/timestamp/Timestamp.java
@@ -3,12 +3,14 @@ package com.team5.first_project.timestamp;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -22,4 +24,11 @@ public class Timestamp {
     @UpdateTimestamp
     @Column(insertable = false)
     private LocalDateTime updatedTime;
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 H시 m분");
+
+    @Transient
+    public String recentTime(){
+        return updatedTime == null ? createdTime.format(formatter) : updatedTime.format(formatter);
+    }
 }
