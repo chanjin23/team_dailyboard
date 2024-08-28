324bf190568c4847327b7fe738f51f66b77d013f
[CLOUD4] 한현 <gusgus1467@naver.com>
2024-08-28 10:49:42 +0900

    Fix : 안 쓰는 라이브러리 삭제

--- first_project/src/main/java/com/team5/first_project/post/controller/PostController.java
+++ first_project/src/main/java/com/team5/first_project/post/controller/PostController.java
@@ -17,9 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
