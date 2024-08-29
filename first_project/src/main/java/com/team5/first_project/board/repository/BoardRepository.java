3980b95d73a2d601392384317f2d0df383b1cbdc
[CLOUD]나용진 <yjnaga@naver.com>
2024-08-29 20:47:17 +0900

    checkNameDuplication

--- first_project/src/main/java/com/team5/first_project/board/repository/BoardRepository.java
+++ first_project/src/main/java/com/team5/first_project/board/repository/BoardRepository.java
@@ -4,8 +4,10 @@ import com.team5.first_project.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findByType(String type);
+    Optional<Board> findByName(String name);
 }
